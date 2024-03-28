b25a313a1f411100a23bedd1aa323348b6b002b3
mabaicun <mabc@nancal.com>
2024-03-29 06:14:30 +0800

    # init

--- internal/server/app.go
+++ internal/server/app.go
@@ -3,7 +3,9 @@ package server
 import (
 	_ "embed"
 	"gorm.io/gorm"
+	"os"
 	"richingm/codeGenerator/internal/conf"
+	"text/template"
 )
 
 var (
@@ -12,16 +14,16 @@ var (
 )
 
 type AppBuild struct {
-	table string
-	data  []gorm.ColumnType
-	conf  conf.Config
+	table  string
+	data   []gorm.ColumnType
+	config conf.Config
 }
 
 func NewAppBuild(table string, data []gorm.ColumnType, config conf.Config) *AppBuild {
 	return &AppBuild{
-		table: table,
-		data:  data,
-		conf:  config,
+		table:  table,
+		data:   data,
+		config: config,
 	}
 }
 
@@ -29,6 +31,51 @@ func (a *AppBuild) Exec() error {
 	return nil
 }
 
-func (a *AppBuild) buildApp() {
+type appBuildData struct {
+	RepoName        string
+	RepoNameToLower string
+	RepoIfName      string
+	RepoPoName      string
+	RepoDoStruct    string
+	TableName       string
+}
 
+func (a *AppBuild) buildAppData() *appBuildData {
+	res := &appBuildData{}
+	//res.RepoName = underscoreToCamelCase(CombineWords(r.table, "Repo"))
+	//res.RepoNameToLower = lowerCamelCase(underscoreToCamelCase(CombineWords(r.table, "Repo")))
+	//res.RepoIfName = underscoreToCamelCase(CombineWords(r.table, "RepoIf"))
+	//res.RepoPoName = underscoreToCamelCase(CombineWords(r.table, "Po"))
+	//res.RepoDoStruct = generateStructFromTable(res.RepoPoName, r.data)
+	res.TableName = a.table
+	return res
+}
+
+func (a *AppBuild) buildApp(data *appBuildData) error {
+	filePath := CombineWords(a.config.WorkDir, a.config.App.RelativePath)
+	fileName := CombineWords(a.table, ".go")
+	return a.buildBase(data, filePath, fileName, appTpl)
+}
+
+func (a *AppBuild) buildBase(data *appBuildData, filePath, fileName, tpl string) error {
+	t := template.Must(template.New("t").Parse(tpl))
+
+	err := CreateFile(filePath, fileName, "")
+	if err != nil {
+		return err
+	}
+
+	fileFullPath := CombineWords(filePath, CombineWords("/", fileName))
+	file, err := os.Create(fileFullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	err = t.Execute(file, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/server/domain.go
+++ internal/server/domain.go
@@ -3,7 +3,9 @@ package server
 import (
 	_ "embed"
 	"gorm.io/gorm"
+	"os"
 	"richingm/codeGenerator/internal/conf"
+	"text/template"
 )
 
 var (
@@ -15,16 +17,16 @@ var (
 )
 
 type DomainBuild struct {
-	table string
-	data  []gorm.ColumnType
-	conf  conf.Config
+	table  string
+	data   []gorm.ColumnType
+	config conf.Config
 }
 
 func NewDomainBuild(table string, data []gorm.ColumnType, config conf.Config) *DomainBuild {
 	return &DomainBuild{
-		table: table,
-		data:  data,
-		conf:  config,
+		table:  table,
+		data:   data,
+		config: config,
 	}
 }
 
@@ -32,10 +34,57 @@ func (d *DomainBuild) Exec() error {
 	return nil
 }
 
-func (d *DomainBuild) buildDo() {
-
+type domainBuildData struct {
+	RepoName        string
+	RepoNameToLower string
+	RepoIfName      string
+	RepoPoName      string
+	RepoDoStruct    string
+	TableName       string
 }
 
-func (d *DomainBuild) buildBiz() {
+func (r *RepoBuild) buildDomainData() *domainBuildData {
+	res := &domainBuildData{}
+	//res.RepoName = underscoreToCamelCase(CombineWords(r.table, "Repo"))
+	//res.RepoNameToLower = lowerCamelCase(underscoreToCamelCase(CombineWords(r.table, "Repo")))
+	//res.RepoIfName = underscoreToCamelCase(CombineWords(r.table, "RepoIf"))
+	//res.RepoPoName = underscoreToCamelCase(CombineWords(r.table, "Po"))
+	//res.RepoDoStruct = generateStructFromTable(res.RepoPoName, r.data)
+	res.TableName = r.table
+	return res
+}
 
+func (d *DomainBuild) buildDo(data *domainBuildData) error {
+	filePath := CombineWords(d.config.WorkDir, d.config.Domain.RelativePath)
+	fileName := CombineWords(d.table, "_do.go")
+	return d.buildBase(data, filePath, fileName, domainDoTpl)
+}
+
+func (d *DomainBuild) buildBiz(data *domainBuildData) error {
+	filePath := CombineWords(d.config.WorkDir, d.config.Domain.RelativePath)
+	fileName := CombineWords(d.table, ".go")
+	return d.buildBase(data, filePath, fileName, domainTpl)
+}
+
+func (d *DomainBuild) buildBase(data *domainBuildData, filePath, fileName, tpl string) error {
+	t := template.Must(template.New("t").Parse(tpl))
+
+	err := CreateFile(filePath, fileName, "")
+	if err != nil {
+		return err
+	}
+
+	fileFullPath := CombineWords(filePath, CombineWords("/", fileName))
+	file, err := os.Create(fileFullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	err = t.Execute(file, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
